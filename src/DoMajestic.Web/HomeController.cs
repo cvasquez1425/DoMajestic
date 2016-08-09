0763b3e6f5bc07e05bba4c44398a7ba5d6f06580
Carlos B. Vasquez <cvasquez1425@hotmail.com>
2016-08-09 14:27:21 -0400

    Add project files.

---
+++ src/DoMajestic.Web/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DoMajestic.Web
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "DoMajestic Home Page";
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            ViewBag.Title = "Services";
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            ViewBag.Title = "Contacts";
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            ViewBag.Title = "Gallery";
+            return View();
+        }
+    }
+}
---
+++ src/DoMajestic.Web/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DoMajestic.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            //UseIISPlatformHandler sets the URL to listen to. This call enables interaction between 
+            // the application and the IIS Environemnt.
+            // The interaction takes place through a component - the IIS HttpPlatformHandler-that you need 
+            // to have installed on your IIS when you deploy the application.
+            //app.UseIISPlatformHandler();
+
+            // The UseStaticFiles() method instead enables your app to serve static files such as images, CSS files stylesheets and scripts.
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
